ad6796d33aff9c5b309c966e59edba36aeb4ca0b
konstantin <kosta123139@gmail.com>
2024-12-20 16:12:17 +0300

    base router and urls

--- vorkers_tz_api/api/api_v1/__init__.py
+++ vorkers_tz_api/api/api_v1/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
+from .worker.urls import router as worker_router
+from .team.urls import router as team_router
 from core import se
 
 api_v1_router = APIRouter()
 
-# api_v1_router.include_router(worker_router, prefix=se.api.v1.worker, tags=["worker"])
-# api_v1_router.include_router(team_router, prefix=se.api.v1.team, tags=["team"])
+api_v1_router.include_router(worker_router, prefix=se.api.v1.worker, tags=["worker"])
+api_v1_router.include_router(team_router, prefix=se.api.v1.team, tags=["team"])
